95f62635a8f5d01fdf41f9c698d4aaf739cd6087
InterAnylastics <72003733+InternetAmethyst@users.noreply.github.com>
2021-07-23 19:15:09 +0530

    k

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,21 +1,21 @@
+import glob
+import os
 import sys
+from pathlib import Path
 from sys import argv
 
 import telethon.utils
-
-from .config import Config
-
+from telethon.tl.functions.channels import InviteToChannelRequest, JoinChannelRequest
+from config import Config
 from . import *
-from .session.main import *
 from .utils import *
+from .session.main import *
 
-
-# let's get the Andencento ready
-async def add_Andencento(Andencento_token):
-    await Andencento.start(Andencento_token)
-    Andencento.me = await Andencento.get_me()
-    Andencento.uid = telethon.utils.get_peer_id(Andencento.me)
-
+# let's get the bot ready                    
+async def add_bot(bot_token):
+    await bot.start(bot_token)
+    Andencento.me = await bot.get_me() 
+    Andencento.uid = telethon.utils.get_peer_id(bot.me)
 
 if len(argv) not in (1, 3, 4):
     Andencento.disconnect()
@@ -25,19 +25,19 @@ else:
         print("CHECKING BOT USERNAME")
         # ForTheGreatrerGood of beautification
         Andencento.tgbot = TelegramClient(
-            "TG_BOT_TOKEN", api_id=Var.APP_ID, api_hash=Var.API_HASH
-        ).start(Andencento_token=Var.BOT_TOKEN)
-        Andencento.loop.run_until_complete(add_Andencento(Var.BOT_TOKEN))
+            "TG_BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH
+        ).start(bot_token=Var.BOT_TOKEN)
+        Andencento.loop.run_until_complete(add_bot(Var.BOT_TOKEN))
         print("CHECKING SUCCESS")
     else:
         Andencento.start()
-
 
 async def mod():
     await asst()
     await plugs()
     await addons()
-
 
 Andencento.loop.run_until_complete(mod())
 Andencento.loop.create_task(op())
